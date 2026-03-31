de040c0562deb7a433a5a61edce552b3a9044a58
AliceDavies27 <215499626+AliceDavies27@users.noreply.github.com>
2026-03-31 08:14:27 +0100

    Fix build script to compile all C files in directory.

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ if not exist build mkdir build
 pushd build
 
 set CompilerFlags=/nologo /MTd /Z7 /Oi /WX /W4 /wd4100 /wd4189
-set SourceFiles=..\src\win32_main.c
+set SourceFiles=..\src\*.c
 set LinkerFlags=/subsystem:windows /incremental:no /opt:ref
 set LibFiles=user32.lib
 
